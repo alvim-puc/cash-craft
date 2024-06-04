e132bc37d8f8e7aae5304ec4287c6ed6a55110ab
alvimdev <alvim103@gmail.com>
2024-06-03 22:52:55 -0300

    feat: estilizando e implementando cadastro | cheque o db.json

--- codigo/public/assets/js/auth.js
+++ codigo/public/assets/js/auth.js
@@ -1,7 +1,7 @@
-import { response } from "express";
-import api from "../../services/api";
+import Api from '../../services/api.js'
+const api = new Api()
 
-function login(username, password) {
+async function login(username, password) {
 
 }
 
@@ -10,13 +10,17 @@ function logout() {
     window.location.href = '/public/index.html'
 }
 
-function signin(username, name, password, email, budget) {
+async function signin(username, name, password, email, budget) {
     var regex = /^(?=(?:.*?[A-Z]){3})(?=(?:.*?[0-9]){2})(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%;*(){}_+^&]*$/;
 
     if(!username || !name || !password || !email || !budget) return alert("Preencha todos os campos")
     if(password.length < 8) return alert("A senha deve ter no mínimo 8 caracteres")
     if(!regex.test(password)) return alert("A senha deve conter pelo menos 3 letras maiúsculas, 2 números e 1 caractere especial")
     if(budget < 0) return alert("O salário não pode ser negativo")
+
+    const clientes = await api.getAllClients()
+    console.log(clientes)
+    if (clientes.some(cliente => cliente.username === username || cliente.email === email)) return alert("Usuário já cadastrado")
 
     const body = {
         "username": username,
@@ -29,8 +33,9 @@ function signin(username, name, password, email, budget) {
         "gastos": [],
     }
 
-    response = api.createClient(body);
-    if (response.statusCode === 200) {
+    const status = await api.createClient(body);
+
+    if (status === "Created") {
         localStorage.setItem('username', username)
         window.location.href = '/public/dashboard.html'
     }
@@ -39,4 +44,12 @@ function signin(username, name, password, email, budget) {
 
 function isAuthenticated() {
     return localStorage.getItem('username') !== null
+}
+
+
+export {
+    login,
+    logout,
+    signin,
+    isAuthenticated
 }
