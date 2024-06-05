383b235de0e950bfe2c81553eb11c1c8fe99d9f9
alvimdev <alvim103@gmail.com>
2024-06-05 12:21:35 -0300

    feat: remodelando db e começando a implementar login

--- codigo/public/assets/js/auth.js
+++ codigo/public/assets/js/auth.js
@@ -1,21 +1,28 @@
 import Api from '../../services/api.js'
 const api = new Api()
 
-async function login(username, password) {
-
+async function login(formData) {
+    const clientes = await api.getAllClients()
+    const cliente = clientes.find(cliente => cliente.email === formData.credential || cliente.username === formData.credential)
+    if(!cliente) return alert("Usuário não encontrado")
+    
+    if(cliente.password === formData.password){
+        
+    }
+    else return alert("Senha inválida")
 }
 
 function logout() {
     localStorage.removeItem('username')
-    window.location.href = '/public/index.html'
+    window.location.href = '/'
 }
 
 async function signin(username, name, password, email, budget) {
-    var regex = /^(?=(?:.*?[A-Z]){3})(?=(?:.*?[0-9]){2})(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%;*(){}_+^&]*$/;
+    var regex = /^(?=(?:.*?[A-Z]){1})(?=(?:.*?[0-9]){2})(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%;*(){}_+^&]*$/;
 
     if(!username || !name || !password || !email || !budget) return alert("Preencha todos os campos")
     if(password.length < 8) return alert("A senha deve ter no mínimo 8 caracteres")
-    if(!regex.test(password)) return alert("A senha deve conter pelo menos 3 letras maiúsculas, 2 números e 1 caractere especial")
+    if(!regex.test(password)) return alert("A senha deve conter pelo menos 1 letra maiúscula, 2 números e 1 caractere especial")
     if(budget < 0) return alert("O salário não pode ser negativo")
 
     const clientes = await api.getAllClients()
@@ -29,15 +36,14 @@ async function signin(username, name, password, email, budget) {
         "senha": password,
         "salario": budget,
         "fixos": [],
-        "ganhos": [],
-        "gastos": [],
+        "lancamentos": []
     }
 
     const status = await api.createClient(body);
 
     if (status === "Created") {
         localStorage.setItem('username', username)
-        window.location.href = '/public/dashboard.html'
+        window.location.href = '/dashboard'
     }
     else alert("Erro ao cadastrar usuário")
 }
