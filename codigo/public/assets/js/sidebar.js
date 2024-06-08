e9385e108db2f3d48d00123a757c3526e62677b6
Athos <99261459+Athoosz@users.noreply.github.com>
2024-06-08 12:49:12 -0300

    Add files via upload

---
+++ codigo/public/assets/js/sidebar.js
@@ -0,0 +1,20 @@
+import { logout, isAuthenticated } from './auth.js'
+
+document.addEventListener("DOMContentLoaded", () => {
+    hamburguer();
+
+    if (!isAuthenticated()) window.location.href = '/login'
+});
+
+function hamburguer() {
+    const hamBurger = document.querySelector(".toggle-btn");
+
+    hamBurger.addEventListener("click", function () {
+        document.querySelector("#sidebar").classList.toggle("expand");
+    });
+}
+
+document.getElementById('logout-btn').addEventListener('click', (e) => {
+    e.preventDefault()
+    logout()
+})
