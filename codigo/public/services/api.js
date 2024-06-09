77acba8e97f025624a44b88d51bf79a7c4419b99
alvimdev <alvim103@gmail.com>
2024-06-09 00:05:48 -0300

    feat: começando rf-04 (crUd de usuário)

--- codigo/public/services/api.js
+++ codigo/public/services/api.js
@@ -27,9 +27,10 @@ class Api {
     }
   }
 
-  async readClient(id) {
+  async readClient(username) {
+    const cliente = (await this.getAllClients()).find(cliente => cliente.username === username)
     try {
-      const response = await fetch(`${this.base}/${id}`, {
+      const response = await fetch(`${this.base}/${cliente.id}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
